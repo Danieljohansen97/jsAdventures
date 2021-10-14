60c0c377f6afd51af01bf624384da9e5aa765644
Danieljohansen97 <danieljohansen1997@hotmail.com>
2021-10-14 22:56:21 +0200

    Add upgrade: Click multiplier

--- src/App.js
+++ src/App.js
@@ -14,16 +14,27 @@ import Functions from "./functions/Functions"
 function App() {
   const [money, setMoney] = useState(0);
   const [clickMultiplier, setClickMultiplier] = useState(1);
+  const [CMPrice, setCMPrice] = useState(10)
 
   const handleClick = () => {
     setMoney(money + 1 * clickMultiplier);
   };
 
+  const handleBuyClickMultiplier = () => {
+    if (money >= CMPrice) {
+      setClickMultiplier(clickMultiplier * 1.5)
+      setMoney(money - CMPrice)
+      setCMPrice(CMPrice * 1.1)
+    } else {
+      alert("Not enough money!")
+    }
+  }
+
   return (
     <div className="App">
       <Container>
         <Row>
-          <h2>{money}</h2>
+          <h2>Money: {Math.round((money + Number.EPSILON) * 100) / 100}</h2>
         </Row>
         <Row>
           <Col></Col>
@@ -40,9 +51,22 @@ function App() {
                 </Card.Header>
                 <Card.Body>
                   <Card.Text>Increase money per click</Card.Text>
+                  <p>{Math.round(CMPrice)}</p>
                   <div className="d-grid gap-2">
-                    <Button onClick={ () => Functions.test() } variant="success">Buy</Button>
-                    <Button onClick={ () => Functions.test2() } variant="success">Buy</Button>
+                    <Button onClick={ handleBuyClickMultiplier } variant="success">Buy x1</Button>
+                    <Button onClick={ () => Functions.test2() } variant="success">Buy x10</Button>
+                  </div>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <Card.Title>Auto Clicker</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Text>Increase money per click</Card.Text>
+                  <div className="d-grid gap-2">
+                    <Button onClick={ () => Functions.test() } variant="success">Buy x1</Button>
+                    <Button onClick={ () => Functions.test2() } variant="success">Buy x10</Button>
                   </div>
                 </Card.Body>
               </Card>
